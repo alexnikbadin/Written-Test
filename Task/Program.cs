fc3fd2fbfbfbc3777f3879480320aee0d0516ecf
Alex.B <badinalexander@icloud.com>
2024-02-26 20:24:58 +0300

    Task

---
+++ Task/Program.cs
@@ -0,0 +1,31 @@
+﻿/*string[] ArrayToArray(string[] array)
+{
+    string[] newArray = new string[2];
+    for (int i = 0; i < array.Length; i++)
+    {
+        newArray[i] = array[i];
+    }
+    return newArray;
+}
+
+string[] x = { "A", "B", "C", "D", "E", "F", "G" };
+Console.WriteLine(ArrayToArray(x));*/
+
+string[] ArrayToArray(string[] array)
+{
+    string[] newArray = new string[3];
+    Array.Copy(array, newArray, 3);
+    return newArray;
+}
+
+string[] x = { "A", "B", "C", "D", "E", "F", "G" };
+
+void PrintChar(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+PrintChar(ArrayToArray(x));
